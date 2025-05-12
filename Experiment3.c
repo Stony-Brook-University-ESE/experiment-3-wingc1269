4a34fd485e4a58719c13576cfb599f95ab5f13a0
Wing Chan <wing.chan@stonybrook.edu>
2025-05-12 14:12:29 -0400

    push

--- Experiment3.c
+++ Experiment3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n = 1000;
-    int matrix[n][n];
+    int *matrix = malloc(n * n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = i * n + j + 1;
+    for (int i = 0; i < n * n; i++) {
+            matrix[i] = i + 1;
         }
-    }
+    
 
     for (int i = n - 1; i >= 0; i--) {
         for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
+            printf("%7d ", matrix[i * n + j]);
         }
         printf("\n");
     }
